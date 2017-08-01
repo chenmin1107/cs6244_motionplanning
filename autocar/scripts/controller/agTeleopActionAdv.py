83a99ce7c913d746f2510436f643e0c8ba3c1cc1
min <chenmin911107@gmail.com>
2017-08-01 19:38:28 +0800

    teleop control

---
+++ autocar/scripts/controller/agTeleopActionAdv.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+# Planner for autonomous vehicle Intersection navigation
+
+# Author:
+# Min CHEN
+
+# SMART: Singapore MIT Alliance for Research and Technology
+
+import  os  
+import  sys  
+import  tty, termios  
+import roslib; roslib.load_manifest('ipma')  
+import rospy 
+import pygame 
+from pygame.locals import * 
+from std_msgs.msg import String  
+from ipma.msg import advisedAction
+
+# global variables topic to be published
+adAct_robot0 = advisedAction()
+adAct_robot1 = advisedAction()
+adAct_robot2 = advisedAction()
+adAct_robot3 = advisedAction()
+adAct_robot4 = advisedAction()
+adActPub_robot0 = rospy.Publisher('/robot_0/advised_action', advisedAction, queue_size=1)
+adActPub_robot1 = rospy.Publisher('/robot_1/advised_action', advisedAction, queue_size=1)
+adActPub_robot2 = rospy.Publisher('/robot_2/advised_action', advisedAction, queue_size=1)
+adActPub_robot3 = rospy.Publisher('/robot_3/advised_action', advisedAction, queue_size=1)
+adActPub_robot4 = rospy.Publisher('/robot_4/advised_action', advisedAction, queue_size=1)
+
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((150, 150))
+  
+def keyboardLoop():  
+    # initialize
+    rospy.init_node('AgActAdvisorTeleop')  
+    rate = rospy.Rate(rospy.get_param('~hz', 10)) 
+    action_pub_ = rospy.get_param('~action_pub', 1) 
+  
+    # show some message  
+    print "Reading from keyboard" 
+    print "Use keys to control the robots"    
+    print "robot_0: w: accelerate, a: decelerate \n \
+        robot_1: up: accelerate, down: decelerate\n\
+        robot_2: r:accelerate, f: decelerate \n\
+        robot_3: y:accelerate, h: decelerate \n\
+        robot_4: pageup: accelerate, pagedown: decelerate"  
+  
+    # read the key   
+    while not rospy.is_shutdown():       
+        clock.tick(50) 
+        keys = pygame.key.get_pressed()
+
+        adAct_robot0.action = action_pub_
+        adAct_robot1.action = action_pub_
+        adAct_robot2.action = action_pub_
+        adAct_robot3.action = action_pub_
+        adAct_robot4.action = action_pub_
+
+        # if keys[pygame.K_w]:
+        #     print 'w has been pressed'
+        # if keys[pygame.K_ESCAPE]:
+        #     print('\nGame Shuting Down!')
+        # if keys[pygame.K_UP]:
+        #     print 'arrow up has been pressed' 
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:sys.exit()
+
+        if(keys[pygame.K_w]):
+            adAct_robot0.action = 0 
+        elif(keys[pygame.K_s]):
+            adAct_robot0.action = 2 
+        if(keys[pygame.K_UP]):
+            adAct_robot1.action = 0 
+        elif(keys[pygame.K_DOWN]):
+            adAct_robot1.action = 2 
+        if(keys[pygame.K_r]):
+            adAct_robot2.action = 0 
+        elif(keys[pygame.K_f]):
+            adAct_robot2.action = 2 
+        if(keys[pygame.K_y]):
+            adAct_robot3.action = 0 
+        elif(keys[pygame.K_h]):
+            adAct_robot3.action = 2 
+        if(keys[pygame.K_PAGEUP]):
+            adAct_robot4.action = 0 
+        elif(keys[pygame.K_PAGEDOWN]):
+            adAct_robot4.action = 2 
+
+
+        # send message  
+        adActPub_robot0.publish(adAct_robot0)
+        adActPub_robot1.publish(adAct_robot1)
+        adActPub_robot2.publish(adAct_robot2)
+        adActPub_robot3.publish(adAct_robot3)
+        adActPub_robot4.publish(adAct_robot4)  
+        rate.sleep()  
+  
+if __name__ == '__main__':  
+    try:  
+        keyboardLoop()  
+    except rospy.ROSInterruptException:  
+        pass  
---
+++ autocar/scripts/controller/controller.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+# Planner for autonomous vehicle Intersection navigation
+
+# Author:
+# Min CHEN
+
+# cs6244: Robot Motion Planning
+
+import  os  
+import  sys  
+import  tty, termios  
+import roslib; roslib.load_manifest('autocar')  
+import rospy 
+import pygame 
+from pygame.locals import * 
+from std_msgs.msg import String  
+from autocar.msg import *
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import *
+from math import *
+import tf
+import numpy as np
+
+class NewtonController:
+    def __init__(self):
+        self.init()
+
+    def init(self):
+
+        self.hz = rospy.get_param('~hz', 10)
+        self.max_speed = rospy.get_param('~max_speed', 5)
+        self.min_speed = rospy.get_param('~min_speed', -3)
+        self.rate = rospy.Rate(self.hz) 
+
+        self.robot_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+
+        self.acc = 0
+        self.yaw = 0
+        self.vel = 0
+
+
+    def command_callback(self, msg):
+        self.acc = msg.acc
+        self.yaw = msg.yaw
+
+
+    def speed_sign(self, msg):
+        quaternion = msg.pose.pose.orientation
+        explicit_quat = [quaternion.x, quaternion.y, quaternion.z, quaternion.w]
+        yaw = tf.transformations.euler_from_quaternion(explicit_quat)[2]
+        yaw_car = np.array([cos(yaw), sin(yaw)])
+
+
+        yaw_vel = atan2(msg.twist.twist.linear.y, msg.twist.twist.linear.x)
+        yaw_vel = np.array([cos(yaw_vel), sin(yaw_vel)])
+
+        if np.dot(yaw_car, yaw_vel) > 0:
+            return 1
+        else:
+            return -1
+        
+
+    def print_odometry_msg(self, msg):
+        quaternion = msg.pose.pose.orientation
+        explicit_quat = [quaternion.x, quaternion.y, quaternion.z, quaternion.w]
+        print 'euler: ', tf.transformations.euler_from_quaternion(explicit_quat)
+        print 'linear: ', msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z
+        print 'vel yaw: ', atan2(msg.twist.twist.linear.y, msg.twist.twist.linear.x)
+        print 'angular: ', msg.twist.twist.angular.x, msg.twist.twist.angular.y, msg.twist.twist.angular.z
+
+    def state_callback(self, msg):
+        self.vel = self.speed_sign(msg) * sqrt(pow(msg.twist.twist.linear.x, 2) + pow(msg.twist.twist.linear.y, 2))
+        
+
+    def update_vel(self):
+        vel = self.vel + self.acc * 1.0 / self.hz
+        if vel > self.max_speed:
+            vel = self.max_speed
+        if vel < self.min_speed:
+            vel = self.min_speed
+        return vel
+
+    def run(self):
+        while not rospy.is_shutdown():
+            vel = self.update_vel()
+            yaw = self.yaw
+            
+            self.send_control(vel, yaw)
+
+            self.rate.sleep()
+            
+    def send_control(self, vel, yaw):
+        msg = Twist()
+        msg.linear.x = vel
+        msg.angular.z = yaw
+        self.robot_pub.publish(msg)
+
+if __name__=='__main__':
+    rospy.init_node('controller')
+
+    controller = NewtonController()
+
+    rospy.Subscriber('control_command', controlCommand, controller.command_callback, queue_size=1)
+    rospy.Subscriber('base_pose_ground_truth', Odometry, controller.state_callback, queue_size=1)
+
+    controller.run()
